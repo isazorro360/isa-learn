eba11c73c8e7be77c4c27cdb217db05a4d2f1eea
ISA <isa@example.com>
2026-06-12 06:40:18 -0700

    Fix Vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/isa-learn/'
-})
+  base: '/isa-learn/',
+  server: {
+    port: 4170,
+    host: true,
+  },
+});
